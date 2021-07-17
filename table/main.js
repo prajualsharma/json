eee33b5267fbaadd18ced376a618d30a78078d33
prajualsharma <64773854+prajualsharma@users.noreply.github.com>
2021-07-17 23:29:39 +0530

    Add files via upload

---
+++ table/main.js
@@ -0,0 +1,14 @@
+$(function(){
+var countryOptions;
+   $.getJSON('countries.json',function(result) {
+       $.each(result, function(i, country) {
+          countryOptions+="<option value='"
+           +country.code+
+            "'>"
+            +country.name+
+             "</option>";
+               });
+              $('#country').html(countryOptions);
+
+      });
+  });
